9e445949dc4fae5741eb05cad126addada6cce4e
s1uzz52 <egorsobakinskikh@mail.ru>
2025-07-06 10:18:28 +0000

    DSFDSFDSFDS

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,20 +6,20 @@ st.set_page_config(
 )
 
 if "theme_dark" not in st.session_state:
-    st.session_state.theme_dark = False 
+    st.session_state.theme_dark = True
+elif "theme_light" not in st.session_state:
+    st.session_state.theme_light = False
 
-col1, col2 = st.columns([6, 1])
+col1, col2 = st.columns([6, 3])
 
-with col1:
-    st.title("❓FAQ")
 with col2:
+    st.title('❓FAQ')
+with col1:
+    st.write('ㅤ')
     if st.session_state.theme_dark:
-        st.write("выбрана темная тема")
+        theme_toggle = st.toggle("темная тема", value=st.session_state.theme_dark, key="theme_dark")
     else:
-        st.write("выбрана светлая тема")
-
-    theme_toggle = st.checkbox("", value=st.session_state.theme_dark, key="theme_dark")
-
+        theme_toggle1 = st.toggle("светлая тема", value=st.session_state.theme_light, key="theme_light")
 
 if st.session_state.theme_dark:
     st.markdown("""
@@ -30,10 +30,20 @@ if st.session_state.theme_dark:
     }
     .stExpanderHeader, .stExpanderContent {
         background-color: #333 !important;
-        color: #eee !important;
+        color: white !important;
         position: relative;
         z-index: 10;
     }
+    /* Стили toggle (checkbox) в темной теме */
+    div[role="switch"] > input[type="checkbox"] + div {
+        background-color: #082BF0 !important;
+        border-color: #082BF0 !important;
+        z-index: 20;
+    }
+    div[role="switch"] > input[type="checkbox"]:checked + div {
+        background-color: #082BF0 !important;
+        border-color: #082BF0 !important;     
+        z-index: 20;
     </style>
     """, unsafe_allow_html=True)
 else:
@@ -47,6 +57,17 @@ else:
         background-color: #f0f0f0 !important;
         color: black !important;
         position: relative;
+        z-index: 10;
+    }
+    /* Стили toggle (checkbox) в светлой теме */
+    div[role="switch"] > input[type="checkbox"] + div {
+        background-color: #000ccc !important;
+        border-color: #000999 !important;
+        z-index: 10;
+    }
+    div[role="switch"] > input[type="checkbox"]:checked + div {
+        background-color: #0d6efd !important;
+        border-color: #0d6efd !important;
         z-index: 10;
     }
     </style>
@@ -64,4 +85,4 @@ with st.expander("Как сохранить qr-код?", expanded=True):
     st.write('С компьютера:\n1. Нажать правой кнопкой мыши по картинке\n' \
     '2. Выбрать пункт "сохранить как"\n')
     st.write('С телефона:\n1. Нажать на картинку\n2. Нажать на 3 точки\n' \
-    '3. Выбрать пункт "сохранить как')
+    '3. Выбрать пункт "сохранить в галерею"')
